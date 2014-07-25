0cfc38925d3f802b7ae4507eb857ac3c765ff0c9
mafro <github@mafro.net>
2014-07-25 16:19:33 +1000

    debian build scripts and http preseed

---
+++ script/saltstack.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -eux
+
+SALT_VERSION=${SALT_VERSION:-latest}
+
+if [[ ${SALT_VERSION:-} == 'latest' ]]; then
+  echo "==> Installing latest Salt version"
+  curl -L http://bootstrap.saltstack.org | sudo sh
+else
+  echo "==> Installing Salt version ${SALT_VERSION}"
+  curl -L http://bootstrap.saltstack.org | sudo sh -s -- git ${SALT_VERSION}
+fi
