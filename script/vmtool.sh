f9f6d862961dd17eefb68f6158ad47c94ca225a3
mafro <github@mafro.net>
2015-03-13 14:09:08 +1100

    vmtool: use rasa's vmware-tools-patches

--- script/vmtool.sh
+++ script/vmtool.sh
@@ -5,15 +5,13 @@ if [[ $PACKER_BUILDER_TYPE =~ vmware ]]; then
     # Assuming the following packages are installed
     # apt-get install -y linux-headers-$(uname -r) build-essential perl
 
+	apt-get install -y git unzip
     cd /tmp
-    mkdir -p /mnt/cdrom
-    mount -o loop /home/vagrant/linux.iso /mnt/cdrom
-    tar zxf /mnt/cdrom/VMwareTools-*.tar.gz -C /tmp/
-    /tmp/vmware-tools-distrib/vmware-install.pl -d
-    rm /home/vagrant/linux.iso
-    umount /mnt/cdrom
-    rmdir /mnt/cdrom
-    rm -rf /tmp/VMwareTools-*
+	git clone https://github.com/rasa/vmware-tools-patches.git
+	cd vmware-tools-patches
+	git checkout download-tools
+	./download-tools.sh 7.1.1
+	./untar-and-patch-and-compile.sh
 fi
 
 if [[ $PACKER_BUILDER_TYPE =~ virtualbox ]]; then
