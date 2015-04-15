fd16ee5bb21f2e36d7d901ba674b0f8d9599eecb
mafro <github@mafro.net>
2015-04-15 17:05:46 +1000

    build-script: support both vmware and virtualbox

--- build-salted-vagrant.sh
+++ build-salted-vagrant.sh
@@ -1,9 +1,10 @@
 #! /bin/bash
 
-USAGE="build-salted-vagrant.sh [-h] [-t] [-d] [-f] -v <version> <flavour>
+USAGE="build-salted-vagrant.sh [-h] [-t] [-d] [-f] [-p platform] -v <version> <flavour>
 
   flavour           release codename {'wheezy','trusty','debian','ubuntu'} ('debian' will default to latest release)
   -v version        salt version tag to install
+  -p (optional)     platform: either virtualbox or vmware
   -t (optional)     test mode (don't dist_upgrade; leave box available for test)
   -d (optional)     debug mode; print all vagrant output
   -h (optional)     print this help message
@@ -14,19 +15,21 @@ VERSION=''
 TEST=0
 DEBUG=0
 FORCE=0
+PLATFORM=vmware
 
 red='\033[0;91m'
 green='\033[0;92m'
 white='\033[0;97m'
 reset='\033[0m'
 
-while getopts "v:tdhf" options
+while getopts "v:tdhfp:" options
 do
 	case $options in
 		v ) VERSION=$OPTARG;;
 		t ) TEST=1;;
 		d ) DEBUG=1;;
 		f ) FORCE=1;;
+		p ) PLATFORM=$OPTARG;;
 		h ) echo "$USAGE" && exit 1;;
 	esac
 done
@@ -81,8 +84,9 @@ function build {
 	local OS=$1			# ubuntu/debian
 	local FLAVOUR=$2	# wheezy/jessie/trusty
 	local VERSION=$3	# v2014.7.1
+	local VM_TYPE=$4	# virtualbox/vmware
 
-	echo "Building Vagrant box for $FLAVOUR at version $VERSION" 1>&3 2>&4
+	echo "Building Vagrant box for $FLAVOUR at version $VERSION on $VM_TYPE" 1>&3 2>&4
 
 	# pass debug flag onto packer
 	if [[ $DEBUG -eq 1 ]]; then PACKER_DEBUG=true; else PACKER_DEBUG=false; fi
@@ -91,7 +95,7 @@ function build {
 	if [[ $TEST -eq 1 ]]; then DIST_UPGRADE=false; else DIST_UPGRADE=true; fi
 
 	# build box image with packer
-	packer build -only=vmware-iso -force \
+	packer build -force -only=$VM_TYPE-iso \
 		-var debug=$PACKER_DEBUG \
 		-var dist_upgrade=$DIST_UPGRADE \
 		-var salt_version="$VERSION" \
@@ -102,7 +106,7 @@ function build {
 	fi
 
 	# create local Vagrantfile for testing
-	create_vagrantfile "$FLAVOUR-packer" $DEBUG
+	create_vagrantfile "$FLAVOUR-$VM_TYPE-packer" $DEBUG
 
 	# stop and destroy an old test build
 	VSTATUS="$(vagrant status)"
@@ -113,19 +117,26 @@ function build {
 
 	# add the box for testing
 	echo "\n${white}==> Adding box to vagrant for testing${reset}" 1>&3 2>&4
-	vagrant box add -f "$FLAVOUR-packer" box/$FLAVOUR-packer.box
+	vagrant box add -f "$FLAVOUR-$VM_TYPE-packer" "box/$FLAVOUR-$VM_TYPE-packer.box"
 
 	# show Vagrant debugging output
 	if [[ $DEBUG -eq 1 ]]; then
 		export VAGRANT_LOG=info
 	fi
 
+	# determine Vagrant provider type
+	if [[ $VM_TYPE == 'vmware' ]]; then
+		PROVIDER_NAME=vmware_desktop
+	else
+		PROVIDER_NAME=$VM_TYPE
+	fi
+
 	# bring up testing box
 	echo "${white}==> Bringing up testing VM with Vagrant${reset}" 1>&3 2>&4
-	vagrant up
+	vagrant up --provider $PROVIDER_NAME
 	if [[ $? -gt 0 ]]; then
 		echo "${red}Failed bringing up the vagrant box!!${reset}" 1>&3 2>&4
-		cleanup "$FLAVOUR-packer" 1
+		cleanup "$FLAVOUR-$VM_TYPE-packer" 1
 		return $?
 	fi
 
@@ -134,7 +145,7 @@ function build {
 	vagrant reload
 	if [[ $? -gt 0 ]]; then
 		echo "${red}Failed reloading the vagrant box!!${reset}" 1>&3 2>&4
-		cleanup "$FLAVOUR-packer" 1
+		cleanup "$FLAVOUR-$VM_TYPE-packer" 1
 		return $?
 	fi
 
@@ -142,14 +153,14 @@ function build {
 	INSTALLED=$(vagrant ssh -c 'salt-call --version' | awk -v version=${VERSION:1} '$1 ~ $version')
 	if [[ -z $INSTALLED ]]; then
 		echo "${red}Failed verifying salt version $VERSION installed on target box!!${reset}" 1>&3 2>&4
-		cleanup "$FLAVOUR-packer" 1
+		cleanup "$FLAVOUR-$VM_TYPE-packer" 1
 		return $?
 	fi
 
 	# remove the testing bits
-	cleanup "$FLAVOUR-packer" 0
+	cleanup "$FLAVOUR-$VM_TYPE-packer" 0
 
-	echo "==> ${green}Verified Salt $VERSION installed on box/$FLAVOUR-packer${reset}" 1>&3 2>&4
+	echo "==> ${green}Verified Salt $VERSION installed on box/$FLAVOUR-$VM_TYPE-packer${reset}" 1>&3 2>&4
 
 	# exit now if using test mode (cleanup was aborted)
 	if [[ $TEST -eq 1 ]]; then
@@ -157,18 +168,18 @@ function build {
 		return 3
 	fi
 
-	if [[ $FORCE -eq 0 ]] && [[ -f "box/${FLAVOUR}64-au-salt-${VERSION}.box" ]]; then
-		read -p "A box exists at box/${FLAVOUR}64-au-salt-${VERSION}.box. Overwrite it? [y/N] " -n1 -s 1>&3 2>&4
+	if [[ $FORCE -eq 0 ]] && [[ -f "box/${FLAVOUR}64-au-salt-${VERSION}-${VM_TYPE}.box" ]]; then
+		read -p "A box exists at box/${FLAVOUR}64-au-salt-${VERSION}-${VM_TYPE}.box. Overwrite it? [y/N] " -n1 -s 1>&3 2>&4
 		echo ''
 	else
 		REPLY=y
 	fi
 
 	if [[ $REPLY =~ ^[Yy]$ ]]; then
-		mv -f "box/$FLAVOUR-packer.box" "box/${FLAVOUR}64-au-salt-${VERSION}.box"
-		echo "${white}==> Box moved to box/${FLAVOUR}64-au-salt-${VERSION}.box${reset}" 1>&3 2>&4
+		mv -f "box/$FLAVOUR-$VM_TYPE-packer.box" "box/${FLAVOUR}64-au-salt-${VERSION}-${VM_TYPE}.box"
+		echo "${white}==> Box moved to box/${FLAVOUR}64-au-salt-${VERSION}-${VM_TYPE}.box${reset}" 1>&3 2>&4
 	else
-		echo "${white}==> Box left at box/$FLAVOUR-packer.box${reset}" 1>&3 2>&4
+		echo "${white}==> Box left at box/$FLAVOUR-${VM_TYPE}-packer.box${reset}" 1>&3 2>&4
 	fi
 
 	echo "==> ${green}Completed build for $FLAVOUR with Salt ${VERSION}${reset}" 1>&3 2>&4
@@ -197,16 +208,16 @@ function cleanup {
 
 # main build dispatch
 if [[ $1 == 'ubuntu' ]]; then
-	build ubuntu trusty $VERSION
+	build ubuntu trusty $VERSION $PLATFORM
 elif [[ $1 == 'trusty' ]]; then
-	build ubuntu trusty $VERSION
+	build ubuntu trusty $VERSION $PLATFORM
 
 elif [[ $1 == 'debian' ]]; then
-	build debian wheezy $VERSION
+	build debian wheezy $VERSION $PLATFORM
 elif [[ $1 == 'wheezy' ]]; then
-	build debian wheezy $VERSION
+	build debian wheezy $VERSION $PLATFORM
 elif [[ $1 == 'jessie' ]]; then
-	build debian jessie $VERSION
+	build debian jessie $VERSION $PLATFORM
 else
 	echo "Unsupported flavour: $1"
 	exit 1
